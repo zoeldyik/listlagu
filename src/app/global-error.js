c1f72f2633306e4347dc0caccc3f0166170e97e9
zoeldyik <zoeldyik@gmail.com>
2024-01-10 14:08:24 +0700

    style change error page

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -3,41 +3,13 @@
 export default function GlobalError({ error, reset }) {
   return (
     <html>
-      <body>
-        <section className="relative z-10 bg-primary py-[120px]">
-          <div className="container mx-auto">
-            <div className="-mx-4 flex">
-              <div className="w-full px-4">
-                <div className="mx-auto max-w-[400px] text-center">
-                  <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
-                    500
-                  </h2>
-                  <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
-                    Ada yang salah nih
-                  </h4>
-                  <p className="mb-8 text-lg text-white">
-                    balik lagi yuk
-                  </p>
-                  <button
-                    onClick={() => reset()}
-                    className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary"
-                  >
-                    reset
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-between space-x-5 md:space-x-8 lg:space-x-14">
-            <div className="h-full w-1/3 bg-gradient-to-t from-[#FFFFFF14] to-[#C4C4C400]"></div>
-            <div className="flex h-full w-1/3">
-              <div className="h-full w-1/2 bg-gradient-to-b from-[#FFFFFF14] to-[#C4C4C400]"></div>
-              <div className="h-full w-1/2 bg-gradient-to-t from-[#FFFFFF14] to-[#C4C4C400]"></div>
-            </div>
-            <div className="h-full w-1/3 bg-gradient-to-b from-[#FFFFFF14] to-[#C4C4C400]"></div>
-          </div>
-        </section>
+      <body className="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
+        <div className="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
+            <p className="text-6xl md:text-7xl lg:text-9xl font-bold tracking-wider text-gray-300">500</p>
+            <p className="text-2xl md:text-3xl lg:text-5xl font-bold tracking-wider text-gray-500 mt-4">Server Error</p>
+            <p className="text-gray-500 mt-8 py-2 border-y-2 text-center">Whoops, something went wrong on our servers.</p>
+            <button className="btn-neutral btn-md" onclick={reset}>Reset</button>
+        </div>
       </body>
     </html>
   )
